3eb43275529795a6e86381b72abf3ca00a1a2447
Lokeshwar Jha <jha.lokeshwar68@gmail.com>
2022-04-26 00:33:09 -0400

    Corrected code for eval calculation

--- app.py
+++ app.py
@@ -4,6 +4,20 @@ import streamlit as st
 st.header("========== WineMakers Dilemna ==========")
 st.caption("Created By: Lokeshwar Jha")
 
+def estimate_value(prob_storm, sensitivity, specificity, p_harvest, p_no_harvest_storm, p_no_harvest_no_storm):
+    
+    p_dns = specificity*(1-prob_storm) + (1-sensitivity)*(1-prob_storm)
+    p_ns_dns = (specificity*prob_storm)/p_dns
+    
+    val_nh_ns = p_ns_dns*p_no_harvest_no_storm
+    val_nh_s = (1-p_ns_dns)*p_no_harvest_storm
+    val_nh = val_nh_ns + val_nh_s
+    
+    val_h = p_harvest*(1-p_dns)
+    
+    total_estimated_value = (val_nh_ns + val_nh_s)*p_dns + val_h
+    
+    return total_estimated_value
 
 def main():
 
@@ -22,15 +36,20 @@ def main():
         e_val_btn = st.button("Calculate e-value")
         if e_val_btn:
             with st.spinner('Calculating e-value....'):
+                p_no_harvest_no_storm = (no_sugar*960 + typical_sugar*1410 + high_sugar*1500)/100
+                p_no_harvest_storm = (botrytis_chance*3300 + (100-botrytis_chance)*420)/100
+                p_harvest = 960
+                eval = estimate_value(prob_storm = 0.5, sensitivity = 0.17, specificity = 0.86,
+                                     p_harvest = p_harvest, p_no_harvest_storm = p_no_harvest_storm,
+                                     p_no_harvest_no_storm = p_no_harvest_no_storm)
                 st.success('Calculation done.')
-                e_val, recommendation = -120, 'Will recommend using the model'
-
-                if e_val > 0:
-                    st.success('E-value: ' + str(e_val) + '$')
-                    st.write(recommendation)
+                
+                if eval > p_harvest:
+                    st.success('E-value: ' + str(eval) + 'k$')
+                    st.write('Will recommend to buy clairvoyance')
                 else:
-                    st.error('E-value: ' + str(e_val) + '$')
-                    st.write(recommendation)
+                    st.error('E-value: ' + str(eval) + 'k$')
+                    st.write('Will not recommend to buy clairvoyance')
 
         
 if __name__ == "__main__":
